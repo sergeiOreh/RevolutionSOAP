9aad8bcbad3f22050c0c5089cfa89ca20a18808f
Sergei <oreshko-sergei@mail.ru>
2020-10-22 16:54:28 +0300

    first commit

---
+++ src/main/java/ru/sergei/oreshko/revolutionsoapwebservice/RevolutionSoapWebServiceApplication.java
@@ -0,0 +1,13 @@
+package ru.sergei.oreshko.revolutionsoapwebservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RevolutionSoapWebServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RevolutionSoapWebServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/sergei/oreshko/revolutionsoapwebservice/RevolutionSoapWebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.sergei.oreshko.revolutionsoapwebservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RevolutionSoapWebServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
